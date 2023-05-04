e239bca65fc44edb7465464defc1983d4c572aaa
Shichao-H <shichao@brandeis.edu>
2023-05-04 14:49:21 -0400

    ca02

---
+++ ca02/start.sh
@@ -0,0 +1,3 @@
+export MONGODB_URI="mongodb+srv://cs103aSpr23:oDyglzBvRw47EJAU@cluster0.kgugl.mongodb.net/?retryWrites=true&w=majority"
+export OPENAI_API_KEY="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"
+npm start
