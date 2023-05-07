ba2d328b08d202e23459eac32f2cbabb29ded5be
Shichao-H <shichao@brandeis.edu>
2023-05-07 19:13:14 -0400

    new api

--- ca02/start.sh
+++ ca02/start.sh
@@ -1,3 +1,3 @@
 export MONGODB_URI="mongodb+srv://cs103aSpr23:oDyglzBvRw47EJAU@cluster0.kgugl.mongodb.net/?retryWrites=true&w=majority"
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY="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"
 npm start
