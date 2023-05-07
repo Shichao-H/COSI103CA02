79624cdbd6a38a22448c576110afa0981354b255
Shichao-H <shichao@brandeis.edu>
2023-05-07 19:15:38 -0400

    delete api key

--- ca02/start.sh
+++ ca02/start.sh
@@ -1,3 +1,2 @@
 export MONGODB_URI="mongodb+srv://cs103aSpr23:oDyglzBvRw47EJAU@cluster0.kgugl.mongodb.net/?retryWrites=true&w=majority"
-export OPENAI_API_KEY="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"
 npm start
